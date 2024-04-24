c1922fb84b4e924925667fad521f183d23bb0f81
Thiago Holz <thiago.coutinho@el.com.br>
2024-04-24 19:17:16 -0300

    Versão 11 - Vinicius

--- ED2024_SC/src/duplamente/ListaDupla.java
+++ ED2024_SC/src/duplamente/ListaDupla.java
@@ -6,41 +6,41 @@ public class ListaDupla {
 	private NoDupla prim;
 	private NoDupla ult;
 	private int quantNos;
-	
+
 	public ListaDupla(){
 		this.prim = null;
 		this.ult = null;
 		this.quantNos = 0;
 	}
-	
+
 	public int getQuantNos(){
 		return this.quantNos;
 	}
-	
+
 	public NoDupla getPrim(){
 		return this.prim;
 	}
-	
+
 	public NoDupla getUlt(){
 		return this.ult;
 	}
-	
+
 	public void setQuantNos(int valorNovo){
 		this.quantNos = valorNovo;
 	}
-	
+
 	public void setPrim(NoDupla novoNo){
 		this.prim = novoNo;
 	}
-	
+
 	public void setUlt(NoDupla novoNo){
 		this.ult = novoNo;
 	}
-	
+
 	public boolean eVazia (){
 		return (this.prim == null);
 	}
-	
+
 	//insere um novo nó no final da lista ou se a lista estiver vazia, insere
 	// o primeiro nó na lista
 	public void inserirUltimo (Item elem){
@@ -54,7 +54,7 @@ public class ListaDupla {
 		this.ult = novoNo;
 		this.quantNos++;
 	}
-	
+
 	//retorna o endereço do nó que está contendo o valor a ser procurado.
 	public NoDupla pesquisarNo (int chave){
 		NoDupla atual = this.prim;
@@ -101,61 +101,204 @@ public class ListaDupla {
 		}
 		return msg;
 	}
-	
-	
+
+
 	// Atividade 04 - Questão 06 
-	
+
 	// Gabarito da Prof
 	public void juntaLista(ListaDupla lista2) {
 		this.ult.setProx(lista2.prim);
 		lista2.prim.setAnt(this.ult);
 		this.ult = lista2.ult;
-		
+
 		lista2.prim = null;
 		lista2.ult = null;
 		this.quantNos += lista2.quantNos;
-		
+
 		lista2.quantNos = 0;
 	}
-	
+
 	// Funciona, mas não é o que ela quer.
 	public void copiaLista (ListaDupla lista2) {
 		NoDupla atualLista2 = lista2.prim;
-		
+
 		while (atualLista2 != null) {
 			int itemLista2 = atualLista2.getInfo().getChave();
-			
+
 			this.inserirUltimo(new Item(itemLista2));
-			
+
 			atualLista2.getProx();
 			atualLista2.setAnt(null);
 		}
-		
+
+	}	
+
+	// Atividade 04 - Questão 07
+	public ListaDupla partirListaAoMeio() {
+		ListaDupla novaLista = new ListaDupla(); // crio uma lista nova;
+		int tamanho = this.quantNos; // tamanho é a divisão do tamanho da lista original;
+		int meio = tamanho / 2; // metade da lista. 
+
+		NoDupla atual = this.prim;
+		int cont = 0;
+
+		while(atual != null && cont < meio) {
+			atual = atual.getProx();
+			cont++;
+		} // enquanto o cont é menor que meio ele ganha mais um;
+
+		novaLista.prim = atual; // nó atual que é o médio aponta para o primeiro;
+		novaLista.ult = this.ult; // 
+		novaLista.quantNos = this.quantNos - meio;
+
+		this.ult = atual.getAnt();
+		this.ult.setProx(null);
+		this.quantNos = meio;
+
+		return novaLista;
+
 	}
-	
-	
+
+
+	// Atividade 04 - Questão 08
+	public int retornarMaiorElem() {
+		if(this.eVazia()) {
+			System.out.println("A lista está vazia.");
+		}
+
+		NoDupla atual = this.prim;
+		int maiorValor = atual.getInfo().getChave();
+
+		while(atual != null) {
+			int valorAtual = atual.getInfo().getChave();
+			if(valorAtual > maiorValor) {
+				maiorValor = valorAtual;
+			}
+			atual = atual.getProx();
+		}
+		return maiorValor;
+	}
+
 	// Atividade 04 - Questão 09
 	public double calcularMedia () {
 		double media = 0;
-		
+
 		NoDupla atual = this.prim;
-		
+
 		if (this.eVazia()) {
 			return -999999;
-			
+
 		} else {
 			while (atual != null) {
 				media += atual.getInfo().getChave();
-				
+
 				atual = atual.getProx();
 			}
-			
+
 			media = media / this.quantNos;
-			
+
 			return media;
 		}
 	}
-	
-	
-	
+
+	// Atividade 04 - Questão 10
+	public void removerZeros() {
+		if (this.eVazia()) {
+			System.out.println("A lista está vazia.");
+			return;
+		}
+
+		NoDupla atual = this.prim;
+
+		while (atual != null) {
+			if (atual.getInfo().getChave() == 0) {
+				// Se o nó atual for o último nó da lista
+				if (atual == this.ult) {
+					this.ult = atual.getAnt();
+				}
+
+				// Se o nó atual for o primeiro nó da lista
+				if (atual == this.prim) {
+					this.prim = atual.getProx();
+					if (this.prim != null) {
+						this.prim.setAnt(null);
+					}
+				} else {
+					NoDupla anterior = atual.getAnt();
+					anterior.setProx(atual.getProx());
+					if (atual.getProx() != null) {
+						atual.getProx().setAnt(anterior);
+					}
+				}
+
+				// Libera o nó atual da memória
+				NoDupla temp = atual;
+				atual = atual.getProx();
+				temp = null;
+
+				// Atualiza a quantidade de nós na lista
+				this.quantNos--;
+			} else {
+				atual = atual.getProx();
+			}
+		}
+
+		if (this.quantNos == 0) {
+			System.out.println("Não foram encontrados valores iguais a 0 na lista.");
+		}
+	}
+
+
+	// Atividade 04 - Questão 11
+	public void removerRepetidos() {
+		if (this.eVazia()) {
+			System.out.println("A lista está vazia.");
+			return;
+		}
+
+		NoDupla atual = this.prim;
+
+		while (atual != null) {
+			int chaveAtual = atual.getInfo().getChave();
+
+			// Verifica se o valor atual já ocorreu antes na lista
+			NoDupla verificar = atual.getProx();
+			while (verificar != null) {
+				if (verificar.getInfo().getChave() == chaveAtual) {
+					// Remove o nó repetido
+					NoDupla anterior = verificar.getAnt();
+					NoDupla proximo = verificar.getProx();
+
+					if (anterior != null) {
+						anterior.setProx(proximo);
+					} else {
+						this.prim = proximo;
+					}
+
+					if (proximo != null) {
+						proximo.setAnt(anterior);
+					} else {
+						this.ult = anterior;
+					}
+
+					NoDupla temp = verificar;
+					verificar = proximo;
+					temp = null;
+
+					this.quantNos--;
+				} else {
+					verificar = verificar.getProx();
+				}
+			}
+			atual = atual.getProx();
+		}
+
+		if (this.quantNos == 0) {
+			System.out.println("Não foram encontrados valores repetidos na lista.");
+		}
+		
+		
+		// Atividade 04 - Questão 12
+	}
+
 }
