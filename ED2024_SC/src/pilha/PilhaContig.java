eef1d006db292db465661d2f0b158de081dfe353
Thiago Holz Coutinho <125706324+Pigas22@users.noreply.github.com>
2024-04-27 00:54:18 +0000

    Ativida 05 - Questões 02 e 03

--- ED2024_SC/src/pilha/PilhaContig.java
+++ ED2024_SC/src/pilha/PilhaContig.java
@@ -40,4 +40,66 @@ public class PilhaContig {
 			return this.info[this.topo];
 		}
 	}
+
+
+	// Atividade 05 - Questão 02
+	public void retirarMaiores10 () {
+		PilhaContig aux = new PilhaContig(this.topo);
+		Item item;
+
+		while (!this.eVazia()) {
+			item = this.desempilhar();
+			if (item.getChave() <= 10) {
+				aux.empilhar(item);
+
+			}
+		}
+
+		while (!aux.eVazia()) {
+			this.empilhar(aux.desempilhar());
+		}
+
+	}
+
+
+	// Atividade 05 - Questão 03
+	public int verificarIdenticas(PilhaContig pilha2) {
+		if (this.eVazia() && pilha2.eVazia()) {
+			return 1;
+		} else {
+			if (this.topo != pilha2.getTopo()) {
+				return 0;
+
+			} else {
+				PilhaContig aux = new PilhaContig(this.topo);
+				PilhaContig aux2 = new PilhaContig(this.topo);
+				Item item, item2;
+				boolean igual = true;				
+
+				while (!this.eVazia() && igual) {
+					item = this.desempilhar();
+					item2 = pilha2.desempilhar();
+
+					if (item.getChave() != item2.getChave()) {
+						igual = false;
+					} else {
+						aux.empilhar(item);
+						aux2.empilhar(item2);
+					}
+				}
+
+				while (!aux.eVazia()) {
+					this.empilhar(aux.desempilhar());
+					pilha2.empilhar(aux2.desempilhar());
+				}
+				if (igual) {
+					return 1;
+
+				} else {
+					return 0;
+
+				}
+			}
+		}
+	}
 }
